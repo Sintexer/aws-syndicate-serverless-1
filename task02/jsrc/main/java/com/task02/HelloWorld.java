98a7e0e46f895d580b04631fa66e2c3e8e0c0bd8
Ilya Buglakov <ilboogl@gmail.com>
2024-04-28 18:15:42 +0300

    fix(task02): change function handler response to JSON

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,6 +2,8 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
@@ -19,13 +21,21 @@ import java.util.Map;
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaUrlConfig(authType = AuthType.NONE, invokeMode = InvokeMode.BUFFERED)
-public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<Map<String, Object>, String> {
 
 	private static final String PATH_KEY = "rawPath";
 
 	private static final String HELLO_MAPPING = "/hello";
 
-	public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
+	public String handleRequest(Map<String, Object> request, Context context) {
+        try {
+            return new ObjectMapper().writeValueAsString(processRequest(request));
+        } catch (JsonProcessingException e) {
+            return "Internal error";
+        }
+    }
+
+	private Map<String, Object> processRequest(Map<String, Object> request) {
 		System.out.println("request: " + request);
 		Object path = request.get(PATH_KEY);
 		System.out.println("path="+path);
